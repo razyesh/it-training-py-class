c42a26540698727599a9203faccba5ca2cbdf337
razyesh <pudasainirajesh504@gmail.com>
2024-01-10 11:15:53 +0545

    add assignment folder

---
+++ assignment/assign1.py
@@ -0,0 +1,9 @@
+# Assignment -> String, List, Tuple, Set , Dict methods
+# Reverse, Sort, index, slicing, remove
+
+# Asks the user to get the input and perform Addition, 
+# Subtraction, Division, Multiplication, power, remainder
+
+# Enter num1, Enter num2 -> 
+
+# Check whats new in Python - theory 
